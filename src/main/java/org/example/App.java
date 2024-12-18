a07246472fd4cbe271edfc2e851c516c8ee0f751
”AnnaRussk” <anna.russkih13@gmail.com>
2024-12-18 12:45:28 +0700

    First structure commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -12,5 +12,23 @@ public class App
         int sum = i++ * ++i;
         System.out.println(sum);
         //System.out.println( "Hello World!" );
+
+        boolean isPassed = true;
+        boolean isStuff = false;
+
+        String enterAccepted = isPassed == true || isStuff == true ? "Открыто" : "Доступ запрещен";
+        System.out.println(enterAccepted);
+
+        String bigAmount = "500";
+        String greatAmount = "100000";
+
+        int hugeAmount = Integer.parseInt(greatAmount) + Integer.parseInt(bigAmount); //напишите тут ваш код
+
+        System.out.println(hugeAmount);
+
+
+
+
     }
+
 }
